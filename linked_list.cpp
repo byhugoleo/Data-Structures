7bbd8a97632702780381d72276dbc3a76883f6ff
byhugoleo <hugoleonardomachado@hotmail.com>
2020-03-29 20:59:22 -0300

    Lista encadeada, sem implementar as funções ou propriedades da classe

---
+++ linked_list.cpp
@@ -0,0 +1,40 @@
+//Implementation of a linked list
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Data {
+//Methods
+    void set_Info(int info){
+        this->info = info;
+    }
+    int get_Info() {
+        return info;
+    }
+
+private:
+//Data-Properties
+    int info;
+};
+
+class node {
+//Data-Properties
+    Data info;
+    node *n;
+
+//Methods
+//TODO
+    
+};
+
+class linked_list {
+//Data-Properties
+//TODO
+
+//Methods
+};
+
+int main()
+{
+//TODO
+}
